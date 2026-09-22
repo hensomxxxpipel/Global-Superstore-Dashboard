44c0e8ae6fe455ca871d5771c46e2ff3eb09fe0f
hensomxxxpipel <rachadrisyh@gmail.com>
2026-09-22 20:08:33 +0700

    first commit

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import numpy as np
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
